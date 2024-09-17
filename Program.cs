371e4c16d5d6239c4bcd8e9619ea13eb29d94812
Rawan_Elsayed <107787557+Merrn60@users.noreply.github.com>
2024-09-17 19:59:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using IV.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Configure endpoints
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "admin",
+    pattern: "{controller=Admin}/{action=AdminDashboard}/{id?}");
+
+app.MapControllerRoute(
+    name: "listVideos",
+    pattern: "Video/ListVideos",
+    defaults: new { controller = "Video", action = "ListVideos" });
+
+app.Run();
